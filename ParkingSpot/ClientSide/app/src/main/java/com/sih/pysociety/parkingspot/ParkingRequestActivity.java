903458ea1e80807f6c449f66575366149c808a96
yashYRS <ysarrof@acm.org>
2019-01-24 01:00:23 +0530

    Places Nearby displayed, filtering results to be done

--- ParkingSpot/ClientSide/app/src/main/java/com/sih/pysociety/parkingspot/ParkingRequestActivity.java
+++ ParkingSpot/ClientSide/app/src/main/java/com/sih/pysociety/parkingspot/ParkingRequestActivity.java
@@ -1,13 +1,41 @@
 package com.sih.pysociety.parkingspot;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ParkingRequestActivity extends AppCompatActivity {
 
+    Button requestParking,logout;
+    Dialog dlg;
+    EditText oldpass,newpass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_request);
+        requestParking = (Button)findViewById(R.id.btn_reqPark);
+        requestParking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent resultsActivity = new Intent(ParkingRequestActivity.this,ResultsActivity.class);
+                startActivity(resultsActivity);
+                finish();
+            }
+        });
+        logout = (Button)findViewById(R.id.btn_logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginActivity = new Intent(ParkingRequestActivity.this,LoginActivity.class);
+                startActivity(loginActivity);
+                finish();
+            }
+        });
     }
 }
+
+
---
+++ ParkingSpot/ClientSide/app/src/main/java/com/sih/pysociety/parkingspot/PlacesRecyclerViewAdapter.java
@@ -0,0 +1,95 @@
+package com.sih.pysociety.parkingspot;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.google.android.gms.location.places.Place;
+
+import java.util.List;
+
+public class PlacesRecyclerViewAdapter extends
+        RecyclerView.Adapter<PlacesRecyclerViewAdapter.ViewHolder> {
+
+    private List<Place> placesList;
+
+    PlacesRecyclerViewAdapter(List<Place> list) {
+        placesList = list;
+
+    }
+    @Override
+    public int getItemCount() {
+        if (placesList.size() < 3)
+            return placesList.size() ;
+        else
+            return  3 ;
+    }
+
+    @NonNull
+    @Override
+    public PlacesRecyclerViewAdapter.ViewHolder
+    onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.places_item, parent, false);
+
+
+        return new PlacesRecyclerViewAdapter.ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(PlacesRecyclerViewAdapter.ViewHolder holder, int position) {
+        final Place place = placesList.get(position);
+        Log.d("show the names ",place.getName().toString() +place.getAddress().toString())  ;
+        holder.name.setText(place.getName());
+        holder.address.setText(place.getAddress());
+        holder.phone.setText(place.getPhoneNumber());
+        if(place.getWebsiteUri() != null){
+            holder.website.setText(place.getWebsiteUri().toString());
+        }
+
+        if(place.getRating() > -1){
+            holder.ratingBar.setNumStars((int)place.getRating());
+        }else{
+            holder.ratingBar.setVisibility(View.GONE);
+        }
+
+        holder.viewOnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Open something
+            }
+        });
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView name;
+        public TextView address;
+        public TextView phone;
+        public TextView website;
+        RatingBar ratingBar;
+
+        Button viewOnMap;
+
+        ViewHolder(View view) {
+
+            super(view);
+            name = view.findViewById(R.id.name);
+            address = view.findViewById(R.id.address);
+            phone = view.findViewById(R.id.phone);
+            website = view.findViewById(R.id.website);
+            ratingBar = view.findViewById(R.id.rating);
+            viewOnMap = view.findViewById(R.id.view_map_b);
+        }
+    }
+
+
+}
