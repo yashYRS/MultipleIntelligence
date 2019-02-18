9c505f25d2784a95ccdfeb2e850ea797635af7d9
DamodarNayak9398 <31372466+DamodarNayak9398@users.noreply.github.com>
2019-02-18 19:42:57 +0530

    BrickBreak with the transition scene
    The folder contains the .jason file which can be opened in GDevelop

---
+++ MultipleIntelligence/Project2/Game/code3.js
@@ -0,0 +1,18 @@
+gdjs.Scene4Code = {};
+
+gdjs.Scene4Code.conditionTrue_0 = {val:false};
+gdjs.Scene4Code.condition0IsTrue_0 = {val:false};
+
+
+gdjs.Scene4Code.eventsList0xb0cf8 = function(runtimeScene) {
+
+}; //End of gdjs.Scene4Code.eventsList0xb0cf8
+
+
+gdjs.Scene4Code.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.Scene4Code.eventsList0xb0cf8(runtimeScene);
+return;
+}
+gdjs['Scene4Code'] = gdjs.Scene4Code;
