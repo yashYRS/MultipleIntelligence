8708ebeb2b25df912d3c657d918f20de8341b6c8
Sush78 <35889186+Sush78@users.noreply.github.com>
2019-02-24 22:29:11 +0530

    Skip button implemented

---
+++ MultipleIntelligence/musicgame/musicword.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import pygame
+import sys
+EXEC_DIR = os.path.dirname(__file__)
+
+class Word(object):
+    def __init__(self, word):
+        if sys.platform == 'darwin':
+            self.word_image = os.path.join('sounds', word)
+        else:
+            self.word_image = os.path.join(EXEC_DIR, 'sounds', word)
+        self.word = word
+        self.image = pygame.mixer.Sound(self.word_image)
+        #self.rect = self.image.get_rect()
+        self.spelling_word = self.word.split('.')[0]
+        self.letters = list(self.spelling_word)
+        self.width = 100
+        self.length = 100
+        
+    '''def draw(self, screen, x, y):
+        screen.blit(self.image, [x, y])'''
